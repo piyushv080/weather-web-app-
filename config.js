27b1f02af18b2b7e5da06efb78ab173505b4c73e
Piyush Verma <65125758+piyushv080@users.noreply.github.com>
2022-02-05 21:31:38 +0530

    configuration files

---
+++ config.js
@@ -0,0 +1,9 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL : "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY : "e16c29e18cfcc8307968c5ce8d2359ef"
+    }
+}
+
+module.exports = constants;
+ 
